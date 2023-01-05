1c0ec812fe4bfa4d47ceffce0143aca5e3e1c07f
David García <davidgarcia.elcau@gmail.com>
2023-01-05 13:34:01 +0100

    Funcionalidad de drag y drop para Planes. Parcialmente implementada para categorías.

--- js/plan.js
+++ js/plan.js
@@ -13,6 +13,12 @@ var cat_counter = 1;
 function addCategory(nombre, planes){
 	let categoria = document.createElement("div");
 	categoria.classList.add("categoria");
+
+	//Permitir drop y drag
+	categoria.setAttribute("draggable", true);
+	categoria.addEventListener("dragover", allowDrop);
+	categoria.addEventListener("drop", drop);
+	
 	let p = document.createElement("p");
 	if(typeof nombre !== 'undefined'){
 		p.innerHTML = nombre;
@@ -37,6 +43,45 @@ function addCategory(nombre, planes){
 	categorias_html.insertBefore(categoria, categorias_html.lastElementChild);
 }
 
+var plan_counter = 1;
 function addPlan(){
-	console.log(this.innerHTML);
+	let plan = document.createElement("div");
+	plan.classList.add("plan");
+
+	//Permitir drag
+	plan.addEventListener("dragstart", drag);
+	plan.setAttribute("draggable", true);
+	
+	//Temporal --Recoger user input
+	plan.innerHTML = "Plan "+plan_counter;
+	plan_counter++;
+
+	this.parentElement.insertBefore(plan, this);
 }
+
+function allowDrop(event){
+	event.preventDefault();
+}
+
+var lastDragged;
+function drag(ev){
+	lastDragged = ev.target;
+}
+
+function drop(ev){//Filtrar segun que se ha movido y donde se quiere soltar
+	//Tambien hay que insertar según la posición relativa donde se suelte
+	if(lastDragged.classList.contains("plan")){//Si arrastramos un plan
+		if(ev.target.classList.contains("plan")){//Sobre otro plan
+			ev.target.parentElement.insertBefore(lastDragged, ev.target.nextSibling);		
+		}
+		if(ev.target.classList.contains("categoria")){//Sobre una categoria
+			ev.target.insertBefore(lastDragged, ev.target.lastElementChild);		
+		}
+	}
+	else if(lastDragged.classList.contains("categoria")){//Si arrastramos una categoria
+		
+	}
+	lastDragged=0;
+}
+
+
